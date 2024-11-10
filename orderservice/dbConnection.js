e79fd9c7f0c244ea2109878a391e9c1d12213ed2
OscarGomezGonzalezz <oscgomgon.business@gmail.com>
2024-11-10 12:57:02 +0100

    comments

--- orderservice/dbConnection.js
+++ orderservice/dbConnection.js
@@ -1,14 +1,13 @@
 // Import the mysql2 package
 const mysql = require('mysql2');
 
-//FIRST WE NEED TO INICIATE THE MYSQL CONTAINER IN DOCKER
-
 // Create a connection pool
 const pool = mysql.createPool({
-  host: 'mysql-service',      // Replace with your database host
-  user: 'node',   // Replace with your database username
-  database: 'database1', // Replace with your database name
-  password: '1234', // Replace with your database password
+  host: 'mysql-service',     //When we directly deploy the mysql container with docker, we are host(localhost),
+                             //but when we deploy it with docker-compose, mysql-container takes the rol of host
+  user: 'node',   
+  database: 'database1', 
+  password: '1234',
   waitForConnections: true
   //port:3306
 });
